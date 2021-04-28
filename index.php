b5c0f71f1e7e9f49d588a9740505be76019c7765
wojciechlasak <wojciech.lasak@outlook.com>
2021-04-28 13:29:34 +0200

    added stamp, added social media, added mobile style

--- index.php
+++ index.php
@@ -10,9 +10,17 @@
     <?php }?>
   </div>
   <div id="flag">
-    <?php
-      include __DIR__ . '/media/flag.svg';
-    ?>
+    <?php include __DIR__ . '/media/flag.svg';?>
+    <div id="stamp">
+      <?php include __DIR__ . '/media/circle.svg';?>
+    </div>
+  </div>
+  <div id="social-media">
+    <div id="social-media-container">
+      <?php include __DIR__ . '/media/fb.svg';?>
+      <?php include __DIR__ . '/media/inst.svg';?>
+    </div>
+    <div id="social-media-text">Cooming soon...</div>
   </div>
 </main>
 <?php include 'footer.php';?>
