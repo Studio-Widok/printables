db7ca39ecbbee57bf242d992e6889ac105e0b19e
wojciechlasak <wojciech.lasak@outlook.com>
2021-04-28 11:25:22 +0200

    styled flag, get images from directory

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php include 'header.php';?>
 <main id="top">
   <div id="slider">
-    <div class="slider-single"
-      style="background-image: url(https://i.picsum.photos/id/889/2000/2000.jpg?hmac=2nMKtJbyjnvQyDW1x428hzhva94dk0uS0vnnHmg5zR8)">
+    <?php
+      $images = glob('media/slider/*.{jpg,png,gif}', GLOB_BRACE);
+      foreach ($images as $image) {
+      ?>
+    <div class="slider-single" style="background-image: url(<?=$image?>)">
     </div>
-    <div class="slider-single"
-      style="background-image: url(https://i.picsum.photos/id/277/2000/2000.jpg?hmac=OS67ixGw6Y_EStldmg8e57XOvwyQKDq0nHrmCXLSYio)">
-    </div>
+    <?php }?>
   </div>
   <div id="flag">
     <?php
-    include __DIR__ . '/media/flag.svg';
-  ?>
+      include __DIR__ . '/media/flag.svg';
+    ?>
   </div>
 </main>
 <?php include 'footer.php';?>
